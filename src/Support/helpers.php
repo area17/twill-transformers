71c2334d4ae1cb8debe6e3f6a2c540dad195c03f
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2020-05-04 14:20:43 +0200

    Fix helpers creation

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,6 +1,9 @@
 <?php
 
-if (function_exists('swap_class')) {
+use A17\Twill\Models\Block as BlockModel;
+use App\Transformers\Block as BlockTransformer;
+
+if (!function_exists('swap_class')) {
     function swap_class($original, $new, $object)
     {
         $original = sprintf('O:%s:"%s"', strlen($original), $original);
@@ -8,5 +11,14 @@ if (function_exists('swap_class')) {
         $new = sprintf('O:%s:"%s"', strlen($new), $new);
 
         return unserialize(str_replace($original, $new, serialize($object)));
+    }
+}
+
+if (!function_exists('_transform')) {
+    function _transform($model)
+    {
+        return $model instanceof BlockModel
+            ? (new BlockTransformer($model))->transform()
+            : $model->transform();
     }
 }

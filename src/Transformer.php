e3e12d14767d69e8fafac72e02e8beda81c26e73
Moufflet Gilbert <gilbert@area17.com>
2021-06-01 22:19:03 +0200

    fix method_exists for PHP 8 only accepts (object|string)

--- src/Transformer.php
+++ src/Transformer.php
@@ -204,9 +204,9 @@ abstract class Transformer implements TransformerContract, ArrayAccess
 
     public function __forwardCallTo($name, $arguments)
     {
-        if (filled($this->data ?? null) && method_exists($this->data, $name)) {
+        if (filled($this->data ?? null) && !is_array($this->data) && method_exists($this->data, $name)) {
             $object = $this->data;
-        } elseif (filled($this->data['data'] ?? null) && method_exists($this->data['data'], $name)) {
+        } elseif (filled($this->data['data'] ?? null) && !is_array($this->data['data']) && method_exists($this->data['data'], $name)) {
             $object = $this->data['data'];
         } elseif (is_array($this->data) && isset($this->data['data'])) {
             $object = $this->data['data'];

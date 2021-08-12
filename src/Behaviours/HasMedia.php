376fdf37e420ff50e145f2a8d2da8ee4632aaff6
Antonio Ribeiro <antonio@area17.com>
2021-08-12 14:14:07 +0200

    Coding style

--- src/Behaviours/HasMedia.php
+++ src/Behaviours/HasMedia.php
@@ -25,11 +25,14 @@ trait HasMedia
     protected function getFirstTranslatedMedia($object)
     {
         return $object->medias->first(function ($media) {
-            return $media->pivot->locale === locale()
-            && $media->pivot->role === $this->role
-            && $media->pivot->crop === $this->crop;
-        }) ?? $object->medias->where('role', $object->role)
-            ->where('role', $object->crop)->first();
+            return $media->pivot->locale === locale() &&
+                $media->pivot->role === $this->role &&
+                $media->pivot->crop === $this->crop;
+        }) ??
+            $object->medias
+                ->where('role', $object->role)
+                ->where('role', $object->crop)
+                ->first();
     }
 
     /**
